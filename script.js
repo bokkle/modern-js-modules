8b9f94a6937df5fe06ce342468fb8b6d16b16493
bokkle <mitch.r_@hotmail.com>
2024-01-19 21:24:39 -0500

    top level await

--- script.js
+++ script.js
@@ -7,6 +7,53 @@ console.log('importing module');
 // addToCart('bread', 5);
 // console.log(price, qt);
 
-import * as ShoppingCart from './shoppingCart.js';
-ShoppingCart.addToCart('bread', 5);
-console.log(ShoppingCart.totalPrice);
+// import * as ShoppingCart from './shoppingCart.js';
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice);
+
+// cant mixed name and default exports, but dont do it!
+// import add, { addToCart, totalPrice as price, qt } from './shoppingCart.js';
+// add('pizza', 2);
+
+import add, { cart, totalPrice } from './shoppingCart.js';
+add('pizza', 2);
+add('bread', 5);
+add('apples', 4);
+
+//IMPORTS ARE NOT COPIES OF THE EXPORT, INSTEAD, THEY ARE A LIVE CONNECTION
+// aka they point to the same place in memory
+console.log(cart);
+
+import { tax } from './shoppingCart.js';
+console.log(tax);
+console.log(totalPrice * tax);
+/*
+// ES2022 AWAIT/MODULES
+// can use await outside of an async function, ONLY WORKS IN MODULES
+console.log('Start fetching');
+//this blocks execution of entire module
+const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+const data = await res.json();
+console.log(data);
+//below will not execute until above does bc blocked by await
+console.log('Something');
+*/
+
+const getLastPost = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  console.log(data);
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+const lastPost = getLastPost();
+console.log(lastPost); // returns a promise bc calling an async func ALWAYS RTNS PROMISE
+
+//this is a workaround (using then on the promise), but it's messy
+lastPost.then((last) => console.log(last));
+
+//instead, use top level await
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
+
+
--- shoppingCart.js
+++ shoppingCart.js
@@ -1,8 +1,13 @@
 // exporting module
 console.log('Exporting module');
 
+//blocking code
+// console.log('Start fetching users');
+// await fetch('https://jsonplaceholder.typicode.com/users');
+// console.log('Finish fetching users');
+
 const shippingCost = 10;
-const cart = [];
+export const cart = [];
 
 // if you want to use code in the main module, need to export
 //exports need to be added in top level code!!!
@@ -15,3 +20,11 @@ const totalPrice = 237;
 const totalQuantity = 23;
 
 export { totalPrice, totalQuantity as qt };
+
+// default exports (used when we want to export 1 thing per module)
+export default (product, quantity) => {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+};
+
+export const tax = 1.13;
